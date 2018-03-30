15471e3c1ce0567fb4c44aeceeb6866495ad8fc8
Joe Higgs <joebh5@gmail.com>
2018-03-30 16:20:52 -0500

    'CORS'

--- src/main/java/com/example/retailtestapi/controller/OrderController.java
+++ src/main/java/com/example/retailtestapi/controller/OrderController.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = { "http://localhost:4200"})
 @RequestMapping("/api")
 public class OrderController {
 
